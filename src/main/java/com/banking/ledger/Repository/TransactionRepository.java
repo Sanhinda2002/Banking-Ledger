9c230780ecc037faf6eb46477b2f79282cf1d451
Sanhinda2002 <sanhindagimhan2002@gmail.com>
2025-06-04 16:11:41 +0530

    add transaction repository

---
+++ src/main/java/com/banking/ledger/Repository/TransactionRepository.java
@@ -0,0 +1,10 @@
+package com.banking.ledger.Repository;
+
+import com.banking.ledger.Models.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TransactionRepository extends JpaRepository <Transaction, Long> {
+    List<Transaction> findByLedgerId(Long ledgerId);
+}
