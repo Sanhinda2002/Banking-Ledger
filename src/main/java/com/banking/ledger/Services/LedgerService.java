f2fe5e624bd68559d60b08a6d65464ec19fb0245
Sanhinda2002 <sanhindagimhan2002@gmail.com>
2025-06-04 16:18:15 +0530

    add ledger service with ledger and transaction repos

--- src/main/java/com/banking/ledger/Services/LedgerService.java
+++ src/main/java/com/banking/ledger/Services/LedgerService.java
@@ -1,4 +1,26 @@
 package com.banking.ledger.Services;
 
+import com.banking.ledger.Repository.LedgerRepository;
+import com.banking.ledger.Repository.TransactionRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class LedgerService {
+    private final LedgerRepository ledgerRepository;
+    private final TransactionRepository transactionRepository;
+
+    public LedgerService (LedgerRepository ledgerRepository, TransactionRepository transactionRepository){
+        this.ledgerRepository = ledgerRepository;
+        this.transactionRepository = transactionRepository;
+    }
+
+    public LedgerRepository getLedgerRepository() {
+        return ledgerRepository;
+    }
+
+    public TransactionRepository getTransactionRepository() {
+        return transactionRepository;
+    }
+
+    
 }
