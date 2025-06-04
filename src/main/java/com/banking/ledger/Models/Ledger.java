edc7847987b0712fef5cec9a92ee7af4c407c5cf
Sanhinda2002 <sanhindagimhan2002@gmail.com>
2025-06-04 12:37:49 +0530

    Update leger.java

--- src/main/java/com/banking/ledger/Models/Ledger.java
+++ src/main/java/com/banking/ledger/Models/Ledger.java
@@ -1,4 +1,15 @@
 package com.banking.ledger.Models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table (name = "ledgers")
 public class Ledger {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private long id;
+    private long userId;
+    private String currency;
+    private String bank;
 }
