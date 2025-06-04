48f62d19e80589abdb77012b37a04d7adf80cf1f
Sanhinda2002 <sanhindagimhan2002@gmail.com>
2025-06-04 23:46:53 +0530

    add getters and setters to ledger

--- src/main/java/com/banking/ledger/Models/Ledger.java
+++ src/main/java/com/banking/ledger/Models/Ledger.java
@@ -12,4 +12,37 @@ public class Ledger {
     private long userId;
     private String currency;
     private String bank;
+
+    public long getId() {
+        return id;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public String getBank() {
+        return bank;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public void setBank(String bank) {
+        this.bank = bank;
+    }
+    
 }
